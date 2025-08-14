39e368ab13c279681485e5c2acaabc2c9958a16d
emilyl2008 <emilylimarib1638@gmail.com>
2025-08-14 07:38:32 -0300

    varias cossitas

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-function teste(){
-  console.log('oioioioi')
-}
-
-
-const cards = document.querySelectorAll('.card');
-
-cards.forEach((card) => {
-  const info = card.querySelector('.info');
-
-  card.addEventListener('mouseenter', () => {
-    info.style.display = 'block';
-  });
-
-  card.addEventListener('mouseleave', () => {
-    info.style.display = 'none';
-  });
-});
-
 const modal = document.querySelector('dialog');
 
 console.log(modal)
